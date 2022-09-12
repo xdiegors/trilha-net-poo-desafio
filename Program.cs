b8eb1645088011c593a24a8376375c35143c5d9f
xdiegors <xdiegors@outlook.com>
2022-09-12 15:53:41 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando o smartphone Nokia");
+var nokia = new Nokia("11988778877", "M2002", "AEAS32378230ZXY", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando o smartphone iPhone");
+var iphone = new Iphone("21977668844", "iPhone 15", "ZXASEWE323237897", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Uber");
